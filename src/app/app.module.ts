8514a94fd64911e0caa212afd86ce922b1c19722
T00064-RD3(林卓志) <T00064@senao.com.tw>
2018-11-11 17:03:37 +0800

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -12,6 +12,7 @@ import { SingupAdvanceComponent } from './singup-advance/singup-advance.componen
   declarations: [AppComponent, SignupComponent, SingupAdvanceComponent],
   imports: [
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
     BrowserModule,
     RouterModule.forRoot([
--- src/app/singup-advance/singup-advance.component.ts
+++ src/app/singup-advance/singup-advance.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
+
+function comfirm(group) {
+  if (group.get('email').value !== group.get('confirmemail').value) {
+    return { emailNotMatch: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-singup-advance',
@@ -6,10 +19,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./singup-advance.component.css']
 })
 export class SingupAdvanceComponent implements OnInit {
+  // formData = new FormGroup({
+  //   name: new FormControl('', [Validators.required]),
+  //   email: new FormControl('table@senao.com.tw'),
+  //   confirmemail: new FormControl({ value: '', disabled: true })
+  // });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
+
+  formData = this.fb.group({
+    name: ['', [Validators.required]],
+    emailGroup: this.fb.group(
+      {
+        email: '',
+        confirmemail: [{ value: '', disabled: true }]
+      },
+      { validator: [comfirm] }
+    )
+  });
 
   ngOnInit() {
+    // 表單初始可以這樣玩
+    // this.http.get('productApi/1').subscribe(productInfo => {
+    //   this.fb.group(productInfo);
+    // });
   }
-
 }
